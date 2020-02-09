cbd722a1dd9f5eb56b50fa3bee5230c45a644bd8
Olawuyi Abdulkareem <47379564+villarnovar@users.noreply.github.com>
2020-02-09 09:37:14 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,38 +1,79 @@
 let submit = document.getElementById("main-button");
 let input = document.getElementById("text2");
-let clear = document.querySelector(".clear")
-let list = document.querySelector("ul");
-let content="";
-let array;
+let clear = document.querySelector("#clear")
+let unorderedList = document.querySelector("ul");
+let taskList;
 
-if(localStorage.getItem("keys")){
-    array = JSON.parse(localStorage.getItem("keys"));
-}else{
-    array = [];
-}
-localStorage.setItem("keys", JSON.stringify(array));
-let data =JSON.parse(localStorage.getItem("keys"));
 
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    if(localStorage.getItem("keys")){
+        taskList = JSON.parse(localStorage.getItem("keys"));
+    }else{
+        taskList = [];
+    }
+    taskList.forEach(eachList=>{
+        let newLi = document.createElement("li");
+        newLi.className = "li";
+        let icon = document.createElement("i");
+        icon.className = "icon";
+        icon.innerHTML = '<i class="fas fa-trash-alt"></i>';
+        newLi.innerText = eachList;
+        newLi.appendChild(icon);
+        unorderedList.appendChild(newLi);
+    })
+});
 
 submit.addEventListener("click", function(e){
-    e.preventDefault();
-    content = input.value;
-    outputList(content);
-    array.push(content);
-    localStorage.setItem("keys", JSON.stringify(array));
-    input.value ="";
+    if(input.value == " "){
+        alert("Type a to-do");
+    }
+        e.preventDefault();
+        let newLi = document.createElement("li");
+        newLi.className = "li";
+        newLi.innerHTML = input.value;
+        let icon = document.createElement("i");
+        icon.className = "icon";
+        icon.innerHTML = '<i class="fas fa-trash-alt"></i>';
+        newLi.appendChild(icon);
+        unorderedList.appendChild(newLi);
+        outputList(input.value);
+        input.value =" ";
 });
-data.forEach(eachKeys=>{
-    outputList(eachKeys);
-})
- 
-function outputList(outputText){
-    let newLi = document.createElement("li");
-    list.appendChild(newLi);
-    newLi.innerText = outputText;
-}
+
+function outputList(saveList){
+    if(localStorage.getItem("keys")){
+        taskList = JSON.parse(localStorage.getItem("keys"));
+    }else{
+        taskList = [];
+    }
+    taskList.push(saveList);
+    localStorage.setItem("keys", JSON.stringify(taskList));
+};
+
+function removesinglelist(singleList){
+    if(localStorage.getItem("keys")){
+        taskList = JSON.parse(localStorage.getItem("keys"));
+    }else{
+        taskList = [];
+    }
+    taskList.forEach(function(eachList, index) {
+        if(singleList.textContent === eachList){
+            taskList.splice(index, 1);
+        }
+    })
+    localStorage.setItem("keys", JSON.stringify(taskList));
+};
+
+unorderedList.addEventListener("click", (icon)=>{
+    if(icon.target.parentElement.classList.contains("icon")){
+        icon.target.parentElement.parentElement.remove();
+    }
+    removesinglelist(icon.target.parentElement.parentElement);
+});
+
 clear.addEventListener("click", ()=>{
+    unorderedList.innerText ="";
     localStorage.clear();
-    list.innerText ="";
-    array = [];
-})
+    taskList = [];
+});
